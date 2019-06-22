df5dfbdaf9dc4dcdf8b865ccf928ae3a1e2512f4
Pushpak Jalan <pushpakjalan07@outlook.com>
2019-06-22 19:54:47 +0530

    Added Numpy Basics 2

---
+++ Numpy Basics 2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([[1,2],[3,4]])
+
+print "Array is of type: ", type(arr), "\n"
+print "Array has dimensions: ", arr.ndim, "\n"
+print "Shape of array: " , arr.shape, "\n"
+print "Size of array: ", arr.size, "\n"
+print "Array stores elements of type: ", arr.dtype, "\n"
